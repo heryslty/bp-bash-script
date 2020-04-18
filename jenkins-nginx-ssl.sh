1efdb81e57edc2aae02ce6787dbef4bf041caf7f
heryslty <hery1923@gmail.com>
2020-04-19 01:43:44 +0700

    update

--- jenkins-nginx-ssl.sh
+++ jenkins-nginx-ssl.sh
@@ -8,7 +8,6 @@ sudo certbot certonly --standalone -d jenkins.heryslty.xyz
 sudo mkdir /etc/nginx/ssl/
 sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
 sudo service nginx stop
-sudo nano /etc/nginx/site-enabled/jenkins.heryslty.xyz
 sudo certbot certonly --standalone -d jenkins.heryslty.xyz 
 sudo nginx -t
 sudo systemctl restart nginx
