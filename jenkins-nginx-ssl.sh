29a2d4d00ae1fa076d0fe223ae2c0aa0cf3c5e28
heryslty <hery1923@gmail.com>
2020-04-19 02:48:13 +0700

    update typo

--- jenkins-nginx-ssl.sh
+++ jenkins-nginx-ssl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo apt install nginx software-properties-common -y
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo add-apt-repository ppa:certbot/certbot
+sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt update
 sudo apt install certbot -y
-sudo certbot certonly --standalone -d jenkins.heryslty.xyz
+sudo certbot certonly --standalone --non-interactive -d jenkins.heryslty.xyz --email hery1923@gmail.com --agree-tos
 sudo mkdir /etc/nginx/ssl/
 sudo openssl dhparam -out /etc/nginx/ssl/dhparam.pem 2048
 sudo service nginx stop
-sudo certbot certonly --standalone -d jenkins.heryslty.xyz 
+sudo certbot certonly --standalone --non-interactive -d jenkins.heryslty.xyz --email hery1923@gmail.com --agree-tos
 sudo nginx -t
 sudo systemctl restart nginx
